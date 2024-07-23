e872dcbab345fbf472a7fa27a68ce8decf0057d7
wowwowster <wowwowster@gmail.com>
2024-07-23 22:25:21 +0200

    Initial commit

---
+++ src/TestComponent.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export function TestComponent() {
+  return "This is a test component";
+}
---
+++ src/index.js
@@ -0,0 +1,25 @@
+import ReactDOM from "react-dom/client";
+import React from "react";
+import { TestComponent } from "./TestComponent";  // Testez avec ce nouveau composant
+
+const rootDiv = document.getElementById("root");
+const reactRoot = ReactDOM.createRoot(rootDiv);
+
+reactRoot.render(<TestComponent />);
+
+
+//importer les outils React
+//import ReactDOM from "react-dom/client";
+//import React from "react";
+
+// importer notre premier composant : App (composant parent le plus haut)
+//import { App } from "./App";
+
+// cibler la div dont l'id est root
+//const rootDiv = document.getElementById("root");
+
+// créer un noeud racine react à partir de la div root (premier noeud de notre DOM virtuel )
+//const reactRoot = ReactDOM.createRoot(rootDiv);
+
+//Injecter notre premier composant le noeud racine
+//reactRoot.render(<App></App>);
